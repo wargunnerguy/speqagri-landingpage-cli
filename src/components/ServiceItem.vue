27b7b10c6d0e92f0347b723c3c94b46144676d17
wargunnerguy <reimovellemaa@gmail.com>
2022-01-13 23:25:20 +0100

    fix dynamic title webpack

--- src/components/ServiceItem.vue
+++ src/components/ServiceItem.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="card" style="width: 18rem;">
-    <img :src="img" class="card-img-top" alt="...">
+    <img :src="img" class="card-img-top" alt="">
     <div class="card-body">
       <h5 class="card-title">{{ name }}</h5>
       <p class="card-text">{{ description }}</p>
-      <a href="#" class="btn btn-primary">Go somewhere</a>
+      <a href="#" class="btn btn-primary">VAATA</a>
     </div>
   </div>
   <div>
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,21 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production' ? '/speqagri-landingpage-cli/' : '/',
-    title: 'SPEQ Agri',
-    pluginOptions: {
-      i18n: {
-        locale: 'et',
-        fallbackLocale: 'en',
-        localeDir: 'lang',
-        enableLegacy: false,
-        runtimeOnly: false,
-        compositionOnly: false,
-        fullInstall: true
-      }
-    }
+	publicPath: process.env.NODE_ENV === 'production' ? '/speqagri-landingpage-cli/' : '/',
+	pages: {
+		index: {
+			// entry for the page
+			entry: 'src/main.js',
+			title: 'CLI SPEQ Agri',
+		},
+	},
+	pluginOptions: {
+		i18n: {
+			locale: 'et',
+			fallbackLocale: 'en',
+			localeDir: 'lang',
+			enableLegacy: false,
+			runtimeOnly: false,
+			compositionOnly: false,
+			fullInstall: true
+		}
+	}
 }
